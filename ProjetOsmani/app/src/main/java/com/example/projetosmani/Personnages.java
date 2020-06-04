a4a88227b2db06d3c776b41a8b5f52f7f5a2e295
Jidrys <57490679+Jidrys@users.noreply.github.com>
2020-06-04 12:48:40 +0200

    Ajout de la classe personnage

---
+++ ProjetOsmani/app/src/main/java/com/example/projetosmani/Personnages.java
@@ -0,0 +1,57 @@
+package com.example.projetosmani;
+
+import android.graphics.drawable.Drawable;
+
+public class Personnages {
+
+    private float  posx ;
+    private float  posy ;
+
+    private int life ;
+    private boolean death ;
+
+    private float width ;
+    private float height ;
+    private Drawable avatar ;
+
+    private int movX ;
+    private int movY ;
+    private int speed ;
+
+    public Personnages(){
+        super();
+    }
+
+    public Personnages(float posx,float posy,float width, float height, int life, int speed) {
+        this.posx = posx ;
+        this.posy = posy ;
+
+        this.width = width ;
+        this.height = height ;
+
+        this.life = life ;
+        this.speed = speed ;
+    }
+
+    public float getX(){
+        return this.posx ;
+    }
+
+    public float getY(){
+        return this.posy ;
+    }
+
+    public void setX(float posx){
+        this.posx = posx ;
+    }
+
+    public void setY(float posy){
+        this.posy = posy ;
+    }
+
+    public void setLife(int life){
+        this.life = life ;
+    }
+
+
+}
