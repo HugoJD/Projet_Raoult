cc7bcd4a42a03cb8a7db2bc98618bd08ec8cb026
HugoJD <62426273+HugoJD@users.noreply.github.com>
2020-06-07 15:47:12 +0200

    Ajout des boutons placés en bas

--- ProjetOsmani/app/src/main/java/com/example/projetosmani/MainActivity.java
+++ ProjetOsmani/app/src/main/java/com/example/projetosmani/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.res.AssetManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.RequiresApi;
 
@@ -13,6 +15,10 @@ import java.io.IOException;
 public class MainActivity extends Activity {
 
     private GameView gameView;
+    private Button BoutonGauche;
+    private Button BoutonDroite;
+    private Button BoutonHaut;
+    private Button BoutonBas;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -31,6 +37,46 @@ public class MainActivity extends Activity {
 
         // et on l'affiche.
         setContentView(gameView);
+        setContentView(R.layout.activity_main);
+        
+        BoutonGauche = (Button) findViewById(R.id.gauche);
+        BoutonDroite = (Button) findViewById(R.id.droite);
+        BoutonBas = (Button) findViewById(R.id.bas);
+        BoutonHaut = (Button) findViewById(R.id.haut);
+
+        BoutonGauche.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //on appelle la méthode pour que Raoult aille à gauche;
+                //raoult.setPosition(raoult.getX()-1; raoult.getY());
+
+            }
+        });
+
+        BoutonDroite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //on appelle la méthode pour que Raoult aille à droite;
+                //raoult.setPosition(raoult.getX()+1; raoult.getY());
+            }
+        });
+
+        BoutonBas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //on appelle la méthode pour que Raoult aille en bas;
+                //raoult.setPosition(raoult.getX(); raoult.getY()-1);
+            }
+        });
+
+        BoutonHaut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //on appelle la méthode pour que Raoult aille en haut;
+                //raoult.setPosition(raoult.getX(); raoult.getY()+1);
+            }
+        });
+
     }
 
 } // class MainActivity
